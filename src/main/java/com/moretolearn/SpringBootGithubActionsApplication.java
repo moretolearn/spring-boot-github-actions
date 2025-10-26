ec1c5a607f71b2998f8de34d0524a3e09261177a
moretolearn <yanumularammohan661@gmail.com>
2025-10-26 12:54:15 +0530

    initial commit

---
+++ src/main/java/com/moretolearn/SpringBootGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.moretolearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGithubActionsApplication.class, args);
+	}
+
+}
